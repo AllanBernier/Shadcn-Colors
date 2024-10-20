01d36d9feebe3e0000243918d52fca975c242f7d
Allan <allan.bernier1@gmail.com>
2024-10-20 21:21:04 +0200

    Fix responsive

--- components/StyleSelector.tsx
+++ components/StyleSelector.tsx
@@ -10,6 +10,7 @@ import { cn } from '@/lib/utils'
 import { ColorPicker } from './ui/color-picker'
 import { ChevronRight, Clipboard } from 'lucide-react'
 import { useToast } from '@/hooks/use-toast'
+import { ScrollArea } from './ui/scroll-area'
 
 export const StyleSelector = ({ setColors }: { setColors(colors: Colors): void }) => {
   const { toast } = useToast()
@@ -62,7 +63,7 @@ export const StyleSelector = ({ setColors }: { setColors(colors: Colors): void }
       title: "Copied To Clipboard",
       description: "Styles successfully copied to clipboard",
     });
-  
+
   }
 
 
@@ -79,96 +80,127 @@ export const StyleSelector = ({ setColors }: { setColors(colors: Colors): void }
         </div>
 
       </div>
-
-
       <div className={cn('overflow-hidden transition-all duration-500 ease-in-out transform md:static absolute z-10 bg-background w-[95vw] md:w-full md:h-fit', open ? 'opacity-100' : 'opacity-0 -translate-x-full')}>
-        <p className="text-sm mb-4">Pick a style and color for your components.</p>
+        <ScrollArea className={'w-auto h-[calc(100vh-64px)]'}>
+          <p className="text-sm mb-4">Pick a style and color for your components.</p>
 
-        <div className="mb-4">
-          <div className='flex justify-between'>
-            <h3 className="font-semibold mb-2">Color</h3>
+          <div className="mb-4">
+            <div className='flex justify-between'>
+              <h3 className="font-semibold mb-2">Color</h3>
 
-            {/* <div className='flex items-center space-x-2'>
+              {/* <div className='flex items-center space-x-2'>
             <Switch id="dark-mode" onCheckedChange={(e) => { toggleTheme(e ? 'dark' : 'light') }} />
             <Label htmlFor="dark-mode">Dark Mode</Label>
           </div> */}
+            </div>
+            <div className="grid grid-cols-3 gap-2">
+              <Button className='hidden bg-zinc-500 bg-slate-500 bg-stone-500 bg-gray-500 bg-neutral-500 bg-red-500 bg-rose-500 bg-orange-500 bg-green-500 bg-blue-500 bg-yellow-500 bg-violet-500'></Button>
+              {['Zinc', 'Slate', 'Stone', 'Gray', 'Neutral', 'Red', 'Rose', 'Orange', 'Green', 'Blue', 'Yellow', 'Violet'].map((color) => (
+                <Button onClick={() => handleColor(color)} key={color} variant="outline" size="sm" className={cn(`bg-${color.toLowerCase()}-500  hover:bg-${color.toLowerCase()}-500/10`, color === defaultColor && `border-white border-2`)}>{color}</Button>
+              ))}
+            </div>
           </div>
-          <div className="grid grid-cols-3 gap-2">
-            <Button className='hidden bg-zinc-500 bg-slate-500 bg-stone-500 bg-gray-500 bg-neutral-500 bg-red-500 bg-rose-500 bg-orange-500 bg-green-500 bg-blue-500 bg-yellow-500 bg-violet-500'></Button>
-            {['Zinc', 'Slate', 'Stone', 'Gray', 'Neutral', 'Red', 'Rose', 'Orange', 'Green', 'Blue', 'Yellow', 'Violet'].map((color) => (
-              <Button onClick={() => handleColor(color)} key={color} variant="outline" size="sm" className={cn(`bg-${color.toLowerCase()}-500  hover:bg-${color.toLowerCase()}-500/10`, color === defaultColor && `border-white border-2`)}>{color}</Button>
-            ))}
+
+
+
+          <div className="mb-4">
+            <h3 className="font-semibold mb-2">Radius</h3>
+            <div className="grid grid-cols-5 gap-2">
+              {['0', '0.3', '0.5', '0.75', '1.0'].map((r) => (
+                <Button onClick={() => handleRaduis(r)} key={r} variant="outline" size="sm" className={r === raduis ? 'bg-primary hover:bg-primary text-primary-foreground' : ''}>{r}</Button>
+              ))}
+            </div>
           </div>
-        </div>
 
-
-
-        <div className="mb-4">
-          <h3 className="font-semibold mb-2">Radius</h3>
-          <div className="grid grid-cols-5 gap-2">
-            {['0', '0.3', '0.5', '0.75', '1.0'].map((r) => (
-              <Button onClick={() => handleRaduis(r)} key={r} variant="outline" size="sm" className={r === raduis ? 'bg-primary hover:bg-primary text-primary-foreground' : ''}>{r}</Button>
-            ))}
+          <div className="mb-4">
+            <h3 className="font-semibold mb-2">Random</h3>
+            <div className="grid grid-cols-2 gap-2">
+              <Button variant="outline" size="sm" className='hover:bg-white hover:text-black bg-white text-black col-span-2'>Generate</Button>
+            </div>
           </div>
-        </div>
 
-        <div className="mb-4">
-          <h3 className="font-semibold mb-2">Random</h3>
-          <div className="grid grid-cols-2 gap-2">
-            <Button variant="outline" size="sm" className='hover:bg-white hover:text-black bg-white text-black col-span-2'>Generate</Button>
+          <div className="mb-4">
+            <h3 className="font-semibold mb-2">Custom</h3>
+            <div className="grid grid-cols-1 gap-2 ">
+
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--background']} onChange={(value) => handleCustom('--background', value)} text='Background'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--card']} onChange={(value) => handleCustom('--card', value)} text='Card'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary']} onChange={(value) => handleCustom('--primary', value)} text='Primary'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--input']} onChange={(value) => handleCustom('--input', value)} text='Input'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--muted-foreground']} onChange={(value) => handleCustom('--muted-foreground', value)} text='Muted'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+              <ColorPicker
+                variant="outline" size="sm"
+                value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
+              />
+
+
+            </div>
           </div>
-        </div>
-
-        <div className="mb-4">
-          <h3 className="font-semibold mb-2">Custom</h3>
-          <div className="grid grid-cols-1 gap-2 ">
-
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--background']} onChange={(value) => handleCustom('--background', value)} text='Background'
-            />
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--card']} onChange={(value) => handleCustom('--card', value)} text='Card'
-            />
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--primary']} onChange={(value) => handleCustom('--primary', value)} text='Primary'
-            />
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--input']} onChange={(value) => handleCustom('--input', value)} text='Input'
-            />
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--muted-foreground']} onChange={(value) => handleCustom('--muted-foreground', value)} text='Muted'
-            />
-            <ColorPicker
-              variant="outline" size="sm"
-              value={customColors['--primary-foreground']} onChange={(value) => handleCustom('--primary-foreground', value)} text='Text'
-            />
 
 
+          <div className="mb-4 block md:hidden">
+            <h3 className="font-semibold mb-2">Custom</h3>
+            <div className="grid grid-cols-1 gap-2 ">
+
+              <Button className='bg-primary block md:hidden justify-end' onClick={() => setOpen(false)}>
+                Close
+              </Button>
+
+              <Button className='bg-primary block md:hidden justify-end' onClick={handleClipboard}>
+                Copy Style
+              </Button>
+
+            </div>
           </div>
-        </div>
-
-
-        <div className="mb-4 block md:hidden">
-          <h3 className="font-semibold mb-2">Custom</h3>
-          <div className="grid grid-cols-1 gap-2 ">
-
-            <Button className='bg-primary block md:hidden justify-end' onClick={() => setOpen(false)}>
-              Close
-            </Button>
-
-            <Button className='bg-primary block md:hidden justify-end' onClick={handleClipboard}>
-              Copy Style
-            </Button>
-
-          </div>
-        </div>
-
+        </ScrollArea>
       </div>
-    </div>
+    </div >
   )
 }
